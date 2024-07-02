3600970da0b11025fa991c616a9058bbf9e580bb
Iskander <iskandersdkv@gmail.com>
2024-07-02 15:00:38 +0300

    FIX: lng on ios

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import '@src/app/index.scss'
 import {BrowserRouter} from "react-router-dom";
-import {Suspense} from "react";
+import {Suspense, useEffect} from "react";
 import {RouterApp} from "@src/app/router";
 import {GlobalProvider} from "@src/app/provider";
 import {Provider} from "react-redux";
@@ -8,8 +8,14 @@ import {store} from "@src/app/redux/store";
 import {PreLoad} from "@src/widgets/pre-load";
 import {SkeletonTheme} from "react-loading-skeleton";
 import 'react-loading-skeleton/dist/skeleton.css'
+import {useTranslation} from "react-i18next";
 
 export const App = () => {
+    const {i18n} = useTranslation()
+    useEffect(() => {
+        i18n.changeLanguage("en-US").then()
+    },[])
+
     return (
         <Suspense fallback={
             <PreLoad words={["posts", "images", "space", "discounts"]}>Loading</PreLoad>
