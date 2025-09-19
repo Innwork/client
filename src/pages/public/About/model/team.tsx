07b199c7a389e909e3e036af7ab7488d30d0c5fe
Hamlet Arakelyan <harakelyan@adnoc.ae>
2025-09-19 20:54:04 +0400

    Team section update

--- src/pages/public/About/model/team.tsx
+++ src/pages/public/About/model/team.tsx
@@ -1,29 +1,28 @@
-import {TeamMember} from "@src/pages/public/About/widgets/our-team/OurTeam";
+import { TeamMember } from "@src/pages/public/About/widgets/our-team/OurTeam";
 import Sos from "@assets/img/About/team/Sos-min.png";
 import Mnatsakan from "@assets/img/About/team/Mnatsakan-min.png";
-import Sahakyan from "@assets/img/About/team/Sahakyan.jpg";
+// import Sahakyan from "@assets/img/About/team/Sahakyan.jpg";
 import Simonyan from "@assets/img/About/team/Simonyan.jpg";
 
-
 export const team: Array<TeamMember> = [
-    {
-        src: Sos,
-        fullName: "Сос Мурадян",
-        post: "Директор основатель"
-    },
-    {
-        src: Mnatsakan,
-        fullName: "Мнацакан Аветисян",
-        post: "Финансовый директор"
-    },
-    {
-        src: Sahakyan,
-        fullName: "Рипсиме Саакян",
-        post: "Менеджер по продажам"
-    },
-    {
-        src: Simonyan,
-        fullName: "Айкуи Симонян",
-        post: "Менеджер по продажам"
-    },
-]
+  {
+    src: Sos,
+    fullName: "Сос Мурадян",
+    post: "Директор основатель",
+  },
+  {
+    src: Mnatsakan,
+    fullName: "Мнацакан Аветисян",
+    post: "Финансовый директор",
+  },
+  // {
+  //     src: Sahakyan,
+  //     fullName: "Рипсиме Саакян",
+  //     post: "Менеджер по продажам"
+  // },
+  {
+    src: Simonyan,
+    fullName: "Айкуи Симонян",
+    post: "Менеджер по продажам",
+  },
+];
