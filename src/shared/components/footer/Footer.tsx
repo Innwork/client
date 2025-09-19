d3060690a403f6fd6693bbe1313801245bc70641
Hamlet Arakelyan <harakelyan@adnoc.ae>
2025-09-19 13:54:59 +0400

    Contact info fixes

--- src/shared/components/footer/Footer.tsx
+++ src/shared/components/footer/Footer.tsx
@@ -1,55 +1,63 @@
-import {useTranslation} from "react-i18next";
-import {footerItems} from "@src/shared/components/footer/constants/constants";
+import { useTranslation } from "react-i18next";
+import { footerItems } from "@src/shared/components/footer/constants/constants";
 import classes from "@src/shared/components/footer/style/footer.module.scss";
-import {TextModule} from "@src/shared/scss";
+import { TextModule } from "@src/shared/scss";
 import LogoBig from "@assets/icons/logo/LogoBig.svg";
-import {FooterAgreements} from "@src/shared/components/footer/widgets/FooterAgreements";
-import {FooterNav} from "@src/shared/components/footer/widgets/FooterNav";
-import {FooterLink} from "@src/shared/components/footer/ui/FooterLink";
+import { FooterAgreements } from "@src/shared/components/footer/widgets/FooterAgreements";
+import { FooterNav } from "@src/shared/components/footer/widgets/FooterNav";
+import { FooterLink } from "@src/shared/components/footer/ui/FooterLink";
+import { useClass } from "@src/shared/hooks";
 
 export const Footer = () => {
-    const {t} = useTranslation('main');
-    return (
-        <footer className={classes.footer_container}>
-            <div className={classes.footer_wrapper}>
-                <div className={classes.footer__grid_container}>
+  const { t } = useTranslation("main");
+  return (
+    <footer className={classes.footer_container}>
+      <div className={classes.footer_wrapper}>
+        <div className={classes.footer__grid_container}>
+          <div className={classes.footer__logo_context}>
+            <LogoBig />
 
-                    <div className={classes.footer__logo_context}>
-                        <LogoBig/>
+            <p className={TextModule.paragraph_white_light}>
+              {t(footerItems.left.text)}
+            </p>
+          </div>
 
-                        <p className={TextModule.paragraph_white_light}>
-                            {t(footerItems.left.text)}
-                        </p>
-                    </div>
+          <FooterNav navName={t(footerItems.center.title)}>
+            {footerItems.center.items.map((el) => (
+              <FooterLink to={el.to} key={el.to}>
+                {t(el.text)}
+              </FooterLink>
+            ))}
+          </FooterNav>
 
-                    <FooterNav navName={t(footerItems.center.title)}>
-                        {footerItems.center.items.map((el) => <FooterLink to={el.to} key={el.to}>
-                            {t(el.text)}
-                        </FooterLink>)}
-                    </FooterNav>
+          <FooterNav navName={t(footerItems.right.title)}>
+            {[
+              footerItems.right.items.map((el) => (
+                <p
+                  className={useClass([
+                    TextModule.paragraph_white_light,
+                    classes.footer_items_right_contacts,
+                  ])}
+                  key={el.text}
+                  dangerouslySetInnerHTML={{ __html: t(el.text) }}
+                />
+              )),
 
+              <div className={classes.footer_social_link} key={"social_links"}>
+                {footerItems.right.socialMedia.map((el) => (
+                  <a href={el.to} target={"_blank"} key={el.to}>
+                    <el.socialMediaSvg />
+                  </a>
+                ))}
+              </div>,
+            ]}
+          </FooterNav>
+        </div>
+      </div>
 
-                    <FooterNav navName={t(footerItems.right.title)}>
-                        {[
-                            footerItems.right.items.map(
-                                (el) => <p className={TextModule.paragraph_white_light} key={el.text}>
-                                    {t(el.text)}
-                                </p>
-                            ),
-
-                            <div className={classes.footer_social_link} key={'social_links'}>
-                                {footerItems.right.socialMedia.map((el) => <a href={el.to} target={'_blank'} key={el.to}>
-                                    <el.socialMediaSvg/>
-                                </a>)}
-                            </div>
-                        ]}
-                    </FooterNav>
-                </div>
-            </div>
-
-            <FooterAgreements orgName='ⓒ innwork'>
-                {footerItems.agreements}
-            </FooterAgreements>
-        </footer>
-    );
+      <FooterAgreements orgName="ⓒ innwork">
+        {footerItems.agreements}
+      </FooterAgreements>
+    </footer>
+  );
 };
--- src/shared/ui/dropdown/Dropdown.tsx
+++ src/shared/ui/dropdown/Dropdown.tsx
@@ -1,31 +1,42 @@
-import {FC} from "react";
-import {DropDownLinkStyle, DropdownStyle, TextModule} from "@src/shared/scss"
-import {DropDownLink} from "@src/shared/ui/link";
-import {ReactNode} from "react";
-import {useTranslation} from "react-i18next";
-import {useClass} from "@src/shared/hooks";
+import { FC } from "react";
+import { DropDownLinkStyle, DropdownStyle, TextModule } from "@src/shared/scss";
+import { DropDownLink } from "@src/shared/ui/link";
+import { ReactNode } from "react";
+import { useTranslation } from "react-i18next";
+import { useClass } from "@src/shared/hooks";
 
 export interface DropdownDownProps {
-    className?: string
-    children?: ReactNode,
-    links?: {path: string, title: string}[]
+  className?: string;
+  children?: ReactNode;
+  links?: { path: string; title: string }[];
 }
 
+export const Dropdown: FC<DropdownDownProps> = ({
+  children,
+  links,
+  className,
+}) => {
+  const { t } = useTranslation("main");
 
-export const Dropdown: FC<DropdownDownProps> = ({children, links, className}) => {
-    const { t } = useTranslation('main')
-
-    return (
-            <nav className={useClass([className, DropdownStyle.dropdown])}>
-                {
-                    links?.map((item) =>
-                      item.path != '' ?
-                        <DropDownLink to={item.path} key={item.title}>{t(item.title)}</DropDownLink>
-                        :
-                        <p key={item.title} className={useClass([DropDownLinkStyle.linkItem, TextModule.paragraph])}>{t(item.title)}</p>
-                    )
-                }
-                {children}
-            </nav>
-    );
+  return (
+    <nav className={useClass([className, DropdownStyle.dropdown])}>
+      {links?.map((item) =>
+        item.path != "" ? (
+          <DropDownLink to={item.path} key={item.title}>
+            {t(item.title)}
+          </DropDownLink>
+        ) : (
+          <p
+            key={item.title}
+            className={useClass([
+              DropDownLinkStyle.linkItem,
+              TextModule.paragraph,
+            ])}
+            dangerouslySetInnerHTML={{ __html: t(item.title) }}
+          />
+        )
+      )}
+      {children}
+    </nav>
+  );
 };
